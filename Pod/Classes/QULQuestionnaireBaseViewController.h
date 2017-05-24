897a1dadb565b00f6e333bee838ecfbaf9e6072b
scott <scott.ph.chou@gmail.com>
2017-05-24 21:52:09 +0800

    Add images & base step view controller

---
+++ Pod/Classes/QULQuestionnaireBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  QULQuestionnaireBaseViewController.h
+//  Pods
+//
+//  Created by Scott Chou on 24/05/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QULQuestionnaireBaseViewController : UIViewController
+
+@end
---
+++ Pod/Classes/QULQuestionnaireBaseViewController.m
@@ -0,0 +1,37 @@
+//
+//  QULQuestionnaireBaseViewController.m
+//  Pods
+//
+//  Created by Scott Chou on 24/05/2017.
+//
+//
+
+#import "QULQuestionnaireBaseViewController.h"
+
+@interface QULQuestionnaireBaseViewController ()
+
+@end
+
+@implementation QULQuestionnaireBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
